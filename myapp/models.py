424268a852cb5b6e087bc98a7bfbe63113f907bc
oleksandrabelmas0908 <oleksandr.abelmas0908@gmail.com>
2025-02-15 16:05:52 +0100

    Add first model, create tables

--- myapp/models.py
+++ myapp/models.py
@@ -1,4 +1,8 @@
 from django.db import models
 
 
-
+class Tour(models.Model):
+    origin_country = models.CharField(max_length=64)
+    destination_country = models.CharField(max_length=64)
+    number_of_nights = models.IntegerField()
+    price = models.IntegerField()
