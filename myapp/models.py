a689a1bc7ea5e82db082eb7e2dd8f8b18a2cdd85
oleksandrabelmas0908 <oleksandr.abelmas0908@gmail.com>
2025-02-16 15:18:39 +0100

    Add template where is shown all objects from tours

--- myapp/models.py
+++ myapp/models.py
@@ -6,3 +6,7 @@ class Tour(models.Model):
     destination_country = models.CharField(max_length=64)
     number_of_nights = models.IntegerField()
     price = models.IntegerField()
+
+
+    def __str__(self):
+        return (f"ID: {self.id} FROM {self.origin_country} TO {self.destination_country}, NUMBER OF NIGHTS: {self.number_of_nights}, PRICE: {self.price}")
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from .models import Tour
 
 
 def index(req):
-    return HttpResponse("Hi!")
+    tours = Tour.objects.all()
+    data = {'tours': tours}
+    return render(req, 'index.html', data)
